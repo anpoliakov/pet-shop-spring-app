8bec637fc73399c84e1819eaeded4056c529c4fa
anpoliakov <Andrewlybimenko@gmail.com>
2020-08-14 22:11:37 +0300

    added a description

--- src/main/java/by/anpoliakov/petshop/Application.java
+++ src/main/java/by/anpoliakov/petshop/Application.java
@@ -4,8 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/*  Использование этой аннотации эквивалентно:
+
+*   @Configuration - позволяет регистрировать дополнительные компоненты (beans) в контексте или импортировать дополнительные классы конфигурации.
+*   @EnableAutoConfiguration - включить механизм автоконфигурации Spring Boot.
+*   @ComponentScan - включить сканирование @Component для пакета, в котором находится приложение.
+*/
+
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class);
+        SpringApplication.run(Application.class, args);
     }
 }
