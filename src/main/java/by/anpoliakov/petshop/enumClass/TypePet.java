d222d318c8def03e900e82c6dc5190c3585bb96c
anpoliakov <Andrewlybimenko@gmail.com>
2020-09-08 22:30:34 +0300

    Added a comment-question

--- src/main/java/by/anpoliakov/petshop/enumClass/TypePet.java
+++ src/main/java/by/anpoliakov/petshop/enumClass/TypePet.java
@@ -3,3 +3,5 @@ package by.anpoliakov.petshop.enumClass;
 public enum TypePet {
     DOG, CAT, ELEPHANT, MONKEY, CHICKEN;
 }
+
+//QUESTION: почему mySQL принимает с JSON только enum ПРОПИСНЫМИ буквами
